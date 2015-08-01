815ddd73f43ecabbe33887ed8fbd793d58293852
River Marmorstein <river@rmarmorstein.com>
2015-08-01 02:14:07 -0400

    changed the perms in the config

---
+++ src/main/java/net/ghostrealms/protect/ConfHelper.java
@@ -0,0 +1,11 @@
+package net.ghostrealms.protect;
+
+public class ConfHelper {
+    
+    public static final String WORLD_EDIT_NODE = "default_edit_perm";
+    public static final String WORLD_USE_NODE = "default_use_perm";
+    public static final String WORLD_ACCESS_NODE = "default_access_perm";
+    public static final String WORLD_MOB_SPAWN_NODE = "default_mob_spawn_perm";
+    public static final String WORLD_PVP_NODE = "default_pvp_perm";
+    public static final String WORLD_EXPLOSION_NODE = "default_explosion_perm";
+}
