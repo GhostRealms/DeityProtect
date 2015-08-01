e20b1356a00199a424a240d19f75e79c37ebc5e4
River Marmorstein <river@rmarmorstein.com>
2015-08-01 02:05:53 -0400

    update paths

---
+++ src/main/java/net/ghostrealms/protect/LangHelper.java
@@ -0,0 +1,27 @@
+package net.ghostrealms.protect;
+
+public class LangHelper {
+    public static final String INVALID_BLOCK_BREAK = "exception_messages.block_break";
+    public static final String INVALID_BLOCK_PLACE = "exception_messages.block_place";
+    public static final String INVALID_SIGN_EDIT = "exception_messages.sign_edit";
+    public static final String INVALID_ENTER = "exception_messages.enter";
+    public static final String INVALID_BUCKET_EMPTY = "exception_messages.bucket_empty";
+    public static final String INVALID_BUCKET_FILL = "exception_messages.bucket_fill";
+    public static final String INVALID_USE_CHEST = "exception_messages.use.chest";
+    public static final String INVALID_USE_DOOR = "exception_messages.use.door";
+    public static final String INVALID_USE_BUTTON = "exception_messages.use.button";
+    public static final String INVALID_USE_LEVER = "exception_messages.use.lever";
+    public static final String INVALID_USE_FURNACE = "exception_messages.use.furnace";
+    public static final String INVALID_USE_BED = "exception_messages.use.bed";
+    public static final String INVALID_USE_PRESSURE_PLATE = "exception_messages.use.pressure_plater";
+    public static final String INVALID_USE_WORKBENCH = "exception_messages.use.workbench";
+    public static final String INVALID_USE_TRAP_DOOR = "exception_messages.use.trap_door";
+    public static final String INVALID_USE_DISPENSER = "exception_messages.use.dispenser";
+    public static final String INVALID_USE_NOTE_BLOCK = "exception_messages.use.note_block";
+    public static final String INVALID_USE_JUKEBOX = "exception_messages.use.jukebox";
+    public static final String INVALID_USE_BOAT = "exception_messages.use.boat";
+    public static final String INVALID_USE_MINECART = "exception_messages.use.minecart";
+    public static final String INVALID_USE_TRIPWIRE = "exception_messages.use.tripwire";
+    public static final String INVALID_USE_ENDERPEARL = "exception_messages.use.enderpearl";
+    public static final String INVALID_FIGHT = "exception_messages.pvp";
+}
